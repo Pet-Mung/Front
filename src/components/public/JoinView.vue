4c70d1241fe8542c23773e712a1cba6f6c1b0717
ssnalee <97poolbbang@naver.com>
2024-07-05 13:03:36 +0900

    footer 수정 0705 13:03

--- src/components/public/JoinView.vue
+++ src/components/public/JoinView.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <p>ss</p>
+    <form>
+        <div class="join_input">
+            <label for="userEmail">이메일</label>
+            <input type="text" id="userEmail" v-model="info.email"/>
+        </div>
+        <div class="join_input">
+            <label for="userName">이름</label>
+            <input type="text" id="userName" v-model="info.username"/>
+        </div>
+        <div class="join_input">
+            <label for="userPw">비밀번호</label>
+            <input type="password" @keyup="pwdCheck" id="userPw" v-model="info.password"/>
+        </div>
+        <div class="join_input">
+            <label for="userPwChk">비밀번호 확인</label>
+            <input type="password" @keyup="pwdCheck" id="userPwChk" v-model="info.password_check"/>
+            <span >{{chkPw ? '비밀번호 일치' : '비밀번호 불일치'}}</span>
+        </div>
+    </form>
     <button type="button" @click="joinBtn">버튼</button>
   </div>
 </template>
@@ -9,6 +27,14 @@
 import { onMounted } from "vue";
 import api from "@/api/userApi.js";
 
+let isInput = false;
+let chkPw = false;
+let info = {
+    password: "",
+    password_check: "",
+    username: "",
+    email: "",
+}
 onMounted(() => {
 
 });
@@ -20,7 +46,6 @@ const createUser = async (info) => {
     console.log(response);
   } catch (error) {
     console.error(error);
-
   }
 };
 const getUsersInfo = async () => {
@@ -32,13 +57,24 @@ const getUsersInfo = async () => {
 
   }
 };
-
+// 비밀번호 확인
+const pwdCheck = () =>{
+    if(info.password !== "" && info.password_check !== "") {
+        isInput = true;
+        if(info.password == info.password_check) chkPw = true;
+        else {chkPw = false; console.log('chkPw',chkPw)}
+    }
+    console.log(info.password)
+    console.log(info.password_check)
+    console.log(isInput)
+};
+// 회원가입 버튼 클릭시
 const joinBtn = () => {
   let info = {
-    "password": "qwer1234!!",
-    "password_check": "qwer1234!!",
-    "username": "asdf123",
-    "email": "user@example.com",
+    password: "qwer1234!!",
+    password_check: "qwer1234!!",
+    username: "qwer",
+    email: "user2@example.com",
   };
   createUser(info);
 getUsersInfo();
