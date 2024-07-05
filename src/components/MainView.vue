dd65c5da2467fd916a725908f4f36432ba28c57a
joyeajin <dpwls31200@gmail.com>
2024-07-05 13:18:57 +0900

    메인페이지 배너

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,13 +1,53 @@
 <template>
-    <div>
-        메인 페이지입니다.
+  <!-- <div>메인 페이지입니다.</div> -->
+  <section>
+    <div class="main-banner">
+      <div class="flex">
+        <div class="text">
+          <p><strong>몸에 좋은 원료를 울 강쥐 사료에</strong> 담다</p>
+          <span>몸에 좋은 이유는 어쩌구저쩌구</span>
+        </div>
+        <img src="@/assets/img/nemo_cat.jpg" alt="" />
+      </div>
     </div>
+  </section>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
+body {
+  margin-bottom: 200px;
+}
+.main_wrap {
+  max-width: none !important;
+  width: 100% !important;
+}
+.flex {
+  display: flex;
+  align-items: center;
+}
+.main-banner {
+  // background-image: url("@/assets/img/cat.jpg");
+  // background-repeat: no-repeat;
+  // background-position: center;
+  // background-size: cover;
+  background-color: bisque;
+  height: 300px;
+  .flex {
+    height: 100%;
 
+    img {
+      height: 100%;
+    }
+  }
+  .text {
+    p {
+      font-size: 32px;
+    }
+    span {
+      font-size: 16px;
+    }
+  }
+}
 </style>
